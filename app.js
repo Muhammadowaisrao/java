bd0e0b9f5f04e390f54026b06ecd2c2954481200
Muhammadowaisrao <rowais283@gmail.com>
2023-02-05 22:45:44 +0500

    upload

---
+++ app.js
@@ -0,0 +1,194 @@
+// // Chapter No-01  "Alert"
+
+// //Question No-01
+// alert("Error!Please Enter ther correct passward.");
+
+// //Question No-02
+// alert("Welcome to JS Land...\nHAppy Codding!");
+
+// //Question No-03
+// alert("Welcome to JS Land...");
+
+// //Question No-04
+// alert("Welcome Coding!  Prevent this page from creating additional dialogs.");
+
+// //Question No-05
+// alert("Hello... I can run JS thrugh my web browser's console");
+
+// //Question No-06
+// // Body (after your page’s HTML)
+
+
+// // Chapter No-02  "Variables For String"
+
+// //Question No-01
+// var UseName;
+
+// //Question No-02
+// var MyName = "Muhammad Owais";
+
+// //Question No-03
+// // a
+// var massage;
+// // b
+// massage = "Hello World";
+// // c
+// alert(massage);
+
+// //Question No-05
+// var ref = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(ref);
+
+// //Question No-07
+// var email = "My email is rowais283@gmail.com";
+// alert(email);
+
+// //Question No-10
+// var java = "Yay! I can write HTML content through JavaScript";
+// document.write(java);
+
+
+// // Chapter No-03  "Variables For Numbers"
+// //Question No-01
+// var age = 14;
+// var old = "I m 14 years old ";
+// alert(old + age);
+
+// //Question No-02
+// var visit = ("You have visited this site 26 times");
+// alert(visit);
+
+// //Question No-03
+// var birthyear;
+// birthday = 1990;
+// var birth = "My Birth Year is";
+// document.write(birth + birthday);
+
+// //Question No-04
+// var VistName = "Usama Khan";
+// var order = 8;
+// var product = "T-Shirt(s)";
+// var store = "www.xyzClothing.com";
+// document.write(VistName + " " + order + " " + product + " " + store);
+
+
+// // Capters Nos-5 to 7     "MATH	EXPRESSIONS"
+// //Question No-01
+// var num1 = 13;
+// var num2 = 9;
+// var add = (13+9);
+// alert("Sum of "+ num1 +" and "+ num2 + " is " + add);
+
+// //Question No-02
+// var num1 = 13;
+// var num2 = 9;
+// var sub = num1-mun2;
+// var mul = num1*num2;
+// var div = num1/num2;
+// var mod = num1%num2;
+// alert("Result of subtracting "+ num2 +" from "+ num1 + " is " + sub);
+// alert("Result of multiplying "+ num1 +" and "+ num2 + " is " + mul);
+// alert("Result of dividing "+ num1 +" by "+ num2 + " is " + div);
+// alert("Remainder of dividing "+ num1 +" from "+ num2 + " is " + mod);
+
+//Question No-04
+// var tic = 600;
+// var buy = 5;
+// var TotCost = tic*buy;
+// document.write("Total cost to buy " + buy + " to a movie is " + TotCost + " PKR");
+
+//Question No-05
+// var num3 = 6;
+// var num4 = 1;
+// var table = num3*num4;
+// document.write("Multiplication of 6" + "<br><br><br>")
+// document.write(num3+ " X " +num4++ + " = " +table+"<br>");
+// var table = num3*num4;
+// document.write(num3+ " X " +num4++ + " = " +table+"<br>");
+// var table = num3*num4;
+// document.write(num3+ " X " +num4++ + " = " +table+"<br>");
+// var table = num3*num4;
+// document.write(num3+ " X " +num4++ + " = " +table+"<br>");
+// var table = num3*num4;
+// document.write(num3+ " X " +num4++ + " = " +table+"<br>");
+// var table = num3*num4;
+// document.write(num3+ " X " +num4++ + " = " +table+"<br>");
+// var table = num3*num4;
+// document.write(num3+ " X " +num4++ + " = " +table+"<br>");
+// var table = num3*num4;
+// document.write(num3+ " X " +num4++ + " = " +table+"<br>");
+// var table = num3*num4;
+// document.write(num3+ " X " +num4++ + " = " +table+"<br>");
+// var table = num3*num4;
+// document.write(num3+ " X " +num4++ + " = " +table+"<br>");
+
+//Question No-07
+// var PrItem1 = 560;
+// var PrItem2 = 200;
+// var Item1 = 2;
+// var Item2 = 5;
+// var Ship = 150;
+// var Totcos = PrItem1*Item1+PrItem2*Item2+Ship;
+// document.write("Shoping Cart"+"<br><br><br>");
+// document.write("Price of Item 1 is "+PrItem1+"<br>");
+// document.write("Quantity of Item 1 is "+Item1+"<br>");
+// document.write("Price of Item 2 is "+PrItem2+"<br>");
+// document.write("Quantity of Item 2 is "+Item2+"<br><br>");
+// document.write("Shippinh Charges is "+Ship+"<br><br>");
+// document.write("Total cost of Youer Order is "+Totcos+"PKR");
+
+// //Question No-08
+// var TotMas = 1200;
+// var ObtMas = 900;
+// var PerMas = ObtMas*100/TotMas;
+// document.write("Marks Sheet"+"<br><br><br>");
+// document.write("Total Marks : "+TotMas+"<br>");
+// document.write("Marks Obtaind : "+ObtMas+"<br>");
+// document.write("Percentage : "+PerMas+"%");
+
+
+// // Capters Nos-8 to 11     "USER	INPUT & CONDITIONAL	STATEMENTS"
+// // //Question No-01
+// var userinput = prompt("Please enter your name");
+// alert("Hi, " + userinput);
+
+
+// //Question No-02
+// var userinput = prompt("Enter a Number to print its multiplication table");
+// var i = 1;
+// document.write(userinput + " X " +i+" = " +userinput*i++ +"<br>");
+// document.write(userinput + " X " +i+" = " +userinput*i++ +"<br>");
+// document.write(userinput + " X " +i+" = " +userinput*i++ +"<br>");
+// document.write(userinput + " X " +i+" = " +userinput*i++ +"<br>");
+// document.write(userinput + " X " +i+" = " +userinput*i++ +"<br>");
+// document.write(userinput + " X " +i+" = " +userinput*i++ +"<br>");
+// document.write(userinput + " X " +i+" = " +userinput*i++ +"<br>");
+// document.write(userinput + " X " +i+" = " +userinput*i++ +"<br>");
+// document.write(userinput + " X " +i+" = " +userinput*i++ +"<br>");
+// document.write(userinput + " X " +i+" = " +userinput*i++ +"<br>");
+
+// // //Question No-03
+// var userinput = +prompt("Please enter Your City");
+//     alert("“Welcome to city of lights”");
+
+// //Question No-04
+// var userinput = +prompt("Please enter gender");
+// var m = "male";
+// var fm = "femail";
+// alert("Good Morning Sir.");
+// alert("Good Morning Ma'am.");
+
+// //Question No-16
+// var userinput = +prompt("insert today temparature");
+// var T = userinput;
+// if(T => 40){
+//     alert("It is too hot outside.");
+// }
+// else if(T =>30){
+//     alert("The Weather today is Normal.")
+// }
+
+
+// // Capters Nos-8 to 11     "USER	INPUT & CONDITIONAL	STATEMENTS"
+// // //Question No-01
+
